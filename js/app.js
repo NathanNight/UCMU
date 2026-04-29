43ce9e37fe6378ccfc4c85725dcdef9765bec9f1
NathanNight <48959358+NathanNight@users.noreply.github.com>
2026-04-29 22:56:24 +0500

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { initAuth } from './auth.js';
-import { initChat } from './chat.js';
-import { initDragFolders } from './drag-folders.js';
-import { initUI } from './ui-panels.js';
-
-initAuth();
-initChat();
-initDragFolders();
-initUI();
