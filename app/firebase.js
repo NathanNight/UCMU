0e1bddae1948fb46294ff8eebd01c617dfd49ef9
NathanNight <48959358+NathanNight@users.noreply.github.com>
2026-05-02 14:50:38 +0500

    Remove Firebase config from tracked auth bridge

--- app/firebase.js
+++ app/firebase.js
@@ -13,16 +13,7 @@ import {
   updateDoc,
   serverTimestamp
 } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyD4d15nd3HJDJCMoi99yjxsBrGWNQ999Y8',
-  authDomain: 'ucmu-63f74.firebaseapp.com',
-  projectId: 'ucmu-63f74',
-  storageBucket: 'ucmu-63f74.firebasestorage.app',
-  messagingSenderId: '475838060478',
-  appId: '1:475838060478:web:af492a673cbffb9e304bef',
-  measurementId: 'G-3M5EFPS2RE'
-};
+import { firebaseConfig } from './firebase-config.local.js';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
